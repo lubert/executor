e53b7ec881ca18661fdd586aea676abfea604b8a
Wolfgang Thaller <wolfgang.thaller@gmx.net>
2023-05-13 00:12:09 +0200

    nix: put the derivation into the flake

--- flake.nix
+++ flake.nix
@@ -7,9 +7,29 @@
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux"  "x86_64-darwin" "aarch64-darwin" ];
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages.default = (import ./. { inherit system; inherit pkgs; });
+        packages.default = pkgs.stdenv.mkDerivation
+          {
+            name = "executor2000";
+            nativeBuildInputs = with pkgs; [ cmake bison perl ruby pkgconfig qt5.wrapQtAppsHook ];
+            buildInputs = with pkgs; [
+              qt5.qtbase
+              boost
+              readline
+              SDL2
+            ] ++ lib.optionals stdenv.isLinux [
+              SDL
+              wayland
+              waylandpp
+            ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
+              Carbon
+              Cocoa
+            ]);
+            src = ./.;
+            hardeningDisable = [ "all" ];
+            cmakeFlags = [ "-DRUN_FIXUP_BUNDLE=NO" "-DNO_STATIC_BOOST=YES" ];
+          };
       };
     };
 }
